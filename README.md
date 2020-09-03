4377449e6833fd50fc16c1812faabf0a4475d4a0
mwisam <wisam878787@gmail.com>
2020-09-03 18:46:33 +0500

    Added banks, countries and fixes

--- README.md
+++ README.md
@@ -1,49 +1,36 @@
-# Maldives Atoll / Island Directory
+# Directory of Maldives Atolls, Islands etc.
 
 [![MIT license](https://img.shields.io/badge/License-MIT-blue.svg)](https://lbesson.mit-license.org/) 
 ![npm](https://img.shields.io/npm/v/mv-directory?style=plastic)
 
-Promise based HTTP client for the browser and node.js
-
-Using this, users can fetch and filter Maldives Atolls and Islands.
+Currenty supports:
+- All Maldivian Atolls and Islands
+- Full Country List.
+- All Maldivian Registered Banks.
 
 ### Installation
 ```sh
-$ npm i mv-directory
+$ npm install mv-directory
 ```
 ### Usage
 Import `MvDirectory` in your JavaScript or TypeScript application.
 ```javascript
 import { MvDirectory } from 'mv-directory';
-```
-#### Examples
-```typescript
 private mvdirectory = new MvDirectory();
 ```
+Example: Fetch All Atolls. ( optional with Island )
 ```typescript
 /**
 * Fetch Atolls
 * @param {boolean} withIslands [false] - optional
 * @param {boolean} withUninhabitedIslands - [false] - optional
 */
-
 const atolls_with_islands = mvdirectory.getAtolls(true);
 ```
+Some Other Examples
 ```typescript
-/**
-* Fetch Atoll Islands
-* @param {string} atollCode
-* @param {boolean} islandName - [undefined] - optional
-*/
-
-const atolls_with_islands = mvdirectory.getAtollIslands("HA");
-```
-```typescript
-/**
-* Fetch All Islands
-*/
-
-const islands = mvdirectory.getAllIslands();
+const atoll = mvdirectory.getAtoll("HA");
+const bank = mvdirectory.getBankByCode("BML");
 ```
 
-Feel free to contribue.
+> Feel free to contribute.
--- ts/src/index.ts
+++ ts/src/index.ts
@@ -1,68 +1,123 @@
 import atolls from "../data/atolls.json";
 import islands from "../data/islands.json";
+import countries from "../data/countries.json";
+import banks from "../data/banks.json";
 // Types
 import Island from "../types/Island";
 import Atoll from "../types/Atoll";
+import Bank from "../types/Bank";
+import Country from "../types/Country";
 
 export default class MvDirectory {
-  getAtolls(withIslands = false, withUninhabitedIslands = false) {
+  private _filterIslands(array: Island[], field: string, code: string) {
+    const data: Island[] = array.filter((item) => item[field] === code);
+    return data;
+  }
+
+  private _filterAtolls(array: Atoll[], field: string, code: string) {
+    const data: Atoll[] = array.filter((item) => item[field] === code);
+    return data;
+  }
+
+  getAtolls(withIslands = false, inhabitedIslandsOnly = false): Atoll[] {
     const atolllist: Atoll[] = atolls;
 
     if (withIslands) {
       atolllist.forEach((atoll) =>
         atoll.islands = this.getAtollIslands(atoll.code)
       );
-      return true;
     }
 
-    if (withUninhabitedIslands) {
+    if (inhabitedIslandsOnly) {
       atolllist.forEach((atoll) =>
-        atoll.islands = this.getAtollIslands(atoll.code, false)
+        atoll.islands = this.getAtollIslands(atoll.code, true)
       );
-      return true;
     }
 
     return atolllist;
   }
 
-  getAtoll(atollCode: string) {
+  getAtoll(
+    atollCode: string,
+    withIslands = false,
+    inhabitedIslandsOnly = false,
+  ): Atoll {
+    if (withIslands) {
+      const atoll = this.getAtollwithIslands(atollCode, inhabitedIslandsOnly);
+      return atoll;
+    }
+
     const atoll = atolls.find((atoll) => atoll.code === atollCode);
+
     return atoll;
   }
 
   getAtollIslands(
     atollCode: string,
-    inhabitedOnly = true,
-    islandName: string | null | undefined = null,
-  ) {
-    let filtered: Island[] = islands.filter((island) =>
-      island.atoll == atollCode
-    );
+    inhabitedOnly = false,
+  ): Island[] {
+    let data: Island[] = this._filterIslands(islands, "atoll", atollCode);
 
     if (inhabitedOnly) {
-      filtered = filtered.filter((island) => island.flag_1 === "I");
+      data = this._filterIslands(data, "flag_1", "I");
     }
 
-    if (islandName != null) {
-      filtered = filtered.filter((island) => island.name === islandName);
-    }
-
-    return filtered;
+    return data;
   }
 
-  getAtollwithIslands(atollCode: string, unInhabited = false) {
+  getAtollwithIslands(atollCode: string, inhabitedOnly = false): Atoll {
     const atolllist: Atoll[] = atolls;
 
     const filtered = atolllist.find((atoll) => atoll.code === atollCode);
-    const islands = this.getAtollIslands(atollCode, unInhabited ? false : true);
+    const islands = this.getAtollIslands(atollCode, inhabitedOnly);
     if (filtered) {
       filtered.islands = islands;
     }
     return filtered;
   }
 
-  getAllIslands() {
-    const allislands: Island[] = islands;
-    return allislands;
+  getAllIslands(): Island[] {
+    const data: Island[] = islands;
+    return data;
+  }
+
+  getIsland(name: string): Island {
+    const island = islands.find((item) => item.name === name);
+    return island;
+  }
+
+  getInhabitedIslands(): Island[] {
+    const data: Island[] = this._filterIslands(islands, "flag_1", "I");
+    return data;
+  }
+
+  getCountries(): Country[] {
+    const data: Country[] = countries;
+    return data;
+  }
+
+  getCountryByCode(countryCode: string): Country {
+    const country = countries.find((country) => country.code === countryCode);
+    return country;
+  }
+
+  getCountryByName(name: string): Country {
+    const country = countries.find((country) => country.name === name);
+    return country;
+  }
+
+  getBanks(): Bank[] {
+    const data: Bank[] = banks;
+    return data;
+  }
+
+  getBankByCode(bankCode: string): Bank {
+    const bank = banks.find((bank) => bank.code === bankCode);
+    return bank;
+  }
+
+  getBankByName(bankName: string): Bank {
+    const bank = banks.find((bank) => bank.name === bankName);
+    return bank;
   }
 }
